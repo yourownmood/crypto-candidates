c072fdf94ea0287bed30be92ff42c84ae61c23fd
Steven Bax <sbax@yourownmood.nl>
2017-05-28 21:15:52 +0200

    Includes better console output + structured parameters

--- README.md
+++ README.md
@@ -2,8 +2,13 @@
 
 A node script to filter cryptocurrencies with potential.
 
-## How to use:
+## First setup:
 
-```javascript
-node index.js
-```
+* Run `npm install`
+* Rename `portfolio.sample.json` as `portfolio.json`.
+* Include the currencies which are currently in your portfolio in `portfolio.json`.
+* Setup your filters in the `Filter parameters:` section of `index.js`
+
+## How to run:
+
+Run `node index.js` in your terminal
--- index.js
+++ index.js
@@ -1,34 +1,75 @@
 'use strict'
+
 const _ = require('lodash')
 const chalk = require('chalk')
 const omitDeep = require('omit-deep')
 const request = require('request')
+const consoleTable = require('console.table')
+const portfolio = require('./portfolio')
 
-const api = 'https://api.coinmarketcap.com/v1/ticker/?convert=EUR'
-const price = 0.01
-const dailyVolume = 100000
-const marketCap = 10000000
-const percentChange7d = -100
+// Filter parameters
+
+const currency = 'EUR' // EUR or USD
+const maxPrice = 0.01
+const minDailyVolume = 100000
+const minMarketCap = 10000000
+const minPercentChange7d = -100
+const currencyException = 'Bitcoin'
+
+// Check currency to exclude
+
+let excludedCurrency
+
+if(currency === 'EUR') {
+  excludedCurrency = 'usd'
+} else {
+  excludedCurrency = 'eur'
+}
+
+// Call api
+
+const api = 'https://api.coinmarketcap.com/v1/ticker/?convert=' + currency.toUpperCase()
 
 request(api, function (error, response, body) {
   if(!error) {
     return filterCandidates(body)
   }
-});
+})
+
+// Filter candidates
 
 function filterCandidates(candidates) {
   candidates = JSON.parse(candidates);
-  candidates = omitDeep(candidates, ['id', 'price_usd', '24h_volume_usd', 'market_cap_usd', 'last_updated'])
+  candidates = omitDeep(candidates, ['id', 'price_' + excludedCurrency, '24h_volume_' + excludedCurrency, 'market_cap_' + excludedCurrency, 'last_updated'])
 
   let myCandidates = _.filter(candidates, function(r) {
-    return r['price_eur'] < price &&
-           r['24h_volume_eur'] > dailyVolume &&
-           r['market_cap_eur'] > marketCap &&
-           r['percent_change_7d'] > percentChange7d
+    return (r['price_eur'] < maxPrice &&
+           r['24h_volume_eur'] > minDailyVolume &&
+           r['market_cap_eur'] > minMarketCap &&
+           r['percent_change_7d'] > minPercentChange7d) || r['name'] === currencyException
   });
 
-  console.log(chalk.bold.white('\nCandidates:\n'))
-  console.log(JSON.stringify(myCandidates, null, 4))
-  console.log(chalk.bold.yellow('\n---------------------------------------------\n'))
-  console.log(chalk.bold.white('Filtered candidates: '), chalk.bold.yellow(myCandidates.length, '\n'))
+  return printCandidates(myCandidates)
+}
+
+function printCandidates(candidates) {
+  console.log(chalk.bold.white('\n---------------------------------------------'))
+  console.log(chalk.bold.white('Candidates:'))
+  console.log(chalk.bold.white('---------------------------------------------'))
+
+  console.table(candidates)
+
+  console.log(chalk.bold.white('\n---------------------------------------------'))
+  console.log(chalk.bold.white('Filtered candidates: '), chalk.bold.yellow(candidates.length))
+  console.log(chalk.bold.white('---------------------------------------------'))
+
+  for(let key in candidates) {
+    if(portfolio.includes(candidates[key].name)) {
+      console.log(chalk.bold.red(candidates[key].name), chalk.grey('owned'))
+    } else {
+      console.log(chalk.bold.green(candidates[key].name))
+    }
+  }
+
+  console.log('\n')
 }
